7a0e08bb7d78aed84ed8a08a2fa82287fc1788fc
danusan-s <141602943+danusan-s@users.noreply.github.com>
2024-10-12 13:35:00 +0800

    Base website with navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,28 @@
-import { useState } from 'react'
+// import { useState } from 'react'
 import './App.css'
 
 function App() {
-    const [count, setCount] = useState(0)
 
     return (
         <>
-            <div>
+            <div id="nav-bar">
+                <a href="#about">About me</a>
+                <a href="#projects">Projects</a>
+                <a href="#interests">Interests</a>
+                <a href="#contact">Contact me</a>
             </div>
+            <div id="main-container">
+                <div id="about" className="sub-section">
+                    <h1 id="hero-text">Hi, my name is Danusan</h1>
+                    <p id="hero-subtitle">I'm a computer engineering undergrad</p>
+                </div>
+                <div id="projects" className="sub-section"></div>
+                <div id="interests" className="sub-section"></div>
+                <div id="contact" className="sub-section"></div>
+            </div>
+            <footer>
+                <p>Color palette inspired by catpucchin</p>
+            </footer>
         </>
     )
 }
